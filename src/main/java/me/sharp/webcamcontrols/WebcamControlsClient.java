d5b3ed78fd1390c0255d5d1f063cef7795e0bc9f
Vincent <vindognz@gmail.com>
2025-01-24 21:08:15 +1300

    init

---
+++ src/main/java/me/sharp/webcamcontrols/WebcamControlsClient.java
@@ -0,0 +1,10 @@
+package me.sharp.webcamcontrols;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WebcamControlsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
